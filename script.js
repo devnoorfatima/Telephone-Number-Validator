760d544526cf9caebb656c14c385732566007c72
devnoorfatima <devnoorfatima@gmail.com>
2019-01-26 18:08:28 +0500

    Added Css and line styling to the html.

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function telephoneCheck(str) {
-    var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+    const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
     return regex.test(str);
  }
- telephoneCheck("555-555-5555");
 
-
+const input = document.getElementById("input");
 
  function check(){
-    var value = document.getElementById("input").value;
+     debugger;
+     console.log(input)
+    let value = input.value;
     document.getElementById("p").innerHTML=telephoneCheck(value);
 }
+input.addEventListener("keypress", function(x){
+if (x.keyCode === 13){
+    check();
+}
+});
